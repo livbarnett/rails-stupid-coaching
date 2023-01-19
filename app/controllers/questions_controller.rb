2a5d2f28961d3bc3db2a9db74904eddb30ed137d
olivia barnett <livbarnett11@gmail.com>
2023-01-19 20:21:59 +0000

    stupid coach

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,6 +6,17 @@ class QuestionsController < ApplicationController
 
   def answer
     @question = params[:answer]
+#     If the message is I am going to work, the coach will answer Great!
+# If the message has a question mark ? at the end, the coach will answer Silly question, get dressed and go to work!.
+# Otherwise the coach will answer I don't care, get dressed and go to work!
+#if, elseif, else
+    if @question == "I am going to work"
+      @answer = "Great!"
+    elsif @question.end_with?("?")
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
   end
 
 end
